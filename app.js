b770d1ea0bca4b9ca83fb9bc28471eb876befaad
Emerson L. Berlik <elberlik@mitre.org>
2018-07-20 01:38:50 -0400

    csv parser

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path')
 var bodyParser = require('body-parser');
 var firebase = require("firebase");
+var fs = require('fs');
+var json2csv = require('json2csv').parse;
+var csvWriter = require('csv-write-stream')
 
 var app = express();
 var router = express.Router();
+
+var newLine= "\r\n";
 
 
 app.use(bodyParser.urlencoded());
@@ -53,6 +58,16 @@ router.route('/entry/:id')
 .post(function(req,res){
    var dataRef = ref.child(req.params.id);
    console.log(req.body);
+   data = {
+       Time: req.body.time,
+       Gx: req.body.gyrox,
+       Gy: req.body.gyroy,
+       Gz: req.body.gyroz,
+       Ax: req.body.accex,
+       Ay: req.body.accey,
+       Az: req.body.accez
+   }
+   add_data_to_csv(data)
    dataRef.push({
      acce: {x: req.body.accex, y: req.body.accey, z: req.body.accez},
      gyro: {x: req.body.gyrox, y: req.body.gyroy, z: req.body.gyroz},
@@ -69,3 +84,12 @@ res.send("success");
     var usersRef = ref.child(req.params.name);
     usersRef.set(null);
 });
+
+add_data_to_csv(data)
+function add_data_to_csv(){
+
+  var writer = csvWriter({sendHeaders: false})
+  writer.pipe(fs.createWriteStream('stolen.csv', {flags: 'a'}))
+  writer.write(data)
+  writer.end()
+}
